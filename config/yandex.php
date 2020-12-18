82dc65ce172474e08b857f13f5dec25bae2aea96
vdmkbu <vdmkbu@ya.ru>
2020-12-18 20:55:05 +0500

    конфиг для xml.yandex

---
+++ config/yandex.php
@@ -0,0 +1,6 @@
+<?php
+    return [
+        'user' => env('XML_YANDEX_USER'),
+        'key' => env('XML_YANDEX_KEY'),
+        'groupby' => 'attr=d.mode=deep.groups-on-page=100.docs-in-group=2'
+    ];
